2256814bc0a0eccc2f234a2443ef38fe97dfd83d
WyoHutch <ghutchinson@digis.net>
2019-10-21 16:37:19 -0600

    Initial commit

---
+++ DragDrp.py
@@ -0,0 +1,32 @@
+import json
+
+def datawrite():
+    data = {}
+    data['people'] = []
+    data['people'].append({
+        'name': 'Scott',
+        'website': 'stackabuse.com',
+        'from': 'Nebraska'
+    })
+    data['people'].append({
+        'name': 'Larry',
+        'website': 'google.com',
+        'from': 'Michigan'
+    })
+    data['people'].append({
+        'name': 'Tim',
+        'website': 'apple.com',
+        'from': 'Alabama'
+    })
+
+def dataread():
+    with open('data.txt') as json_file:
+        data = json.load(json_file)
+        for p in data['people']:
+            print('Name: ' + p['name'])
+            print('Website: ' + p['website'])
+            print('From: ' + p['from'])
+            print('')
+
+datawrite()
+dataread()
