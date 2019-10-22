6a24bb1c5e001de79bdc318ea52a770dcc0c36d9
WyoHutch <ghutchinson@digis.net>
2019-10-22 10:47:30 -0600

    Created PostgreSQL database

--- app.py
+++ app.py
@@ -1,34 +1,81 @@
-import json
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_marshmallow import Marshmallow
+from flask_heroku import Heroku
+import os
 
-def datawrite():
-    data = {}
-    data['people'] = []
-    data['people'].append({
-        'name': 'Scott',
-        'website': 'stackabuse.com',
-        'from': 'Nebraska'
-    })
-    data['people'].append({
-        'name': 'Larry',
-        'website': 'google.com',
-        'from': 'Michigan'
-    })
-    data['people'].append({
-        'name': 'Tim',
-        'website': 'apple.com',
-        'from': 'Alabama'
-    })
-    with open('data.txt', 'w') as outfile:
-        json.dump(data, outfile)
+app = Flask(__name__)
+heroku = Heroku(app)
 
-def dataread():
-    with open('data.txt') as json_file:
-        data = json.load(json_file)
-        for p in data['people']:
-            print('Name: ' + p['name'])
-            print('Website: ' + p['website'])
-            print('From: ' + p['from'])
-            print('')
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://iqiszpzxrnecrl:82b0c1758a3ab17a0e8adcd666f52c939e0224bd25625304d6ee2e97e273b754@ec2-174-129-241-114.compute-1.amazonaws.com:5432/dbhr6c772m4dtj"
 
-datawrite()
-dataread()
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+CORS(app)
+
+class Student(db.Model):
+    __tablename__ = "students"
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(20), unique = False)
+    team = db.Column(db.String(20), unique = False)
+
+    def __init__(self, name, team):
+        self.name = name
+        self.team = team
+
+class StudentSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "name", "team")
+
+single_jschema = StudentSchema()
+plural_jschema = StudentSchema(many = True)
+
+@app.route('/student', methods=['POST'])
+def add_student():
+    name = request.json["name"]
+    team = request.json["team"]
+
+    new_student = Student(name, team)
+
+    db.session.add(new_student)
+    db.session.commit()
+
+    return single_jschema.jsonify(Student.query.get(new_student.id))
+
+@app.route('/Students', methods=['GET'])
+def get_Students():
+    all_students = Student.query.all()
+    return jsonify(plural_jschema.dump(all_students))
+
+@app.route('/getStudent/<id>', methods=['GET'])
+def return_student(id):
+    student = Student.query.get(id)
+    return jsonify(single_jschema.dump(student))
+
+@app.route('/student/<id>', methods=['PUT'])
+def update_student(id):
+    student = Student.query.get(id)
+
+    student.name = request.json('name')
+    student.team = request.json('team')
+
+    db.session.commit()
+    return single_jschema.jsonify(student)
+
+@app.route('/delStudent/<id>', methods=['DELETE'])
+def delete_student(id):
+    student = Student.query.get(id)
+    db.session.delete(student)
+    db.session.commit()
+
+    all_students = Student.query.all()
+    return jsonify(plural_jschema.dump(all_students))
+
+name = "Hayley"
+team = 1
+
+add_student
+if __name__ == '__main__':
+    app.run(debug = True)
